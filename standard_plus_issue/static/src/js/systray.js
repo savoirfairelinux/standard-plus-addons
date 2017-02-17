dba18955318e72326cbbe71abc4b959fd81bb7d1
Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
2017-02-17 17:41:58 -0500

    [FIX] Support button (retry)

--- standard_plus_issue/static/src/js/systray.js
+++ standard_plus_issue/static/src/js/systray.js
@@ -1,3 +1,8 @@
+/**
+ * © 2017 Savoir-faire Linux
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+ */
+
 odoo.define('standard_plus_issue.systray', function (require) {
 "use strict";
 
@@ -6,6 +11,18 @@ var Widget = require('web.Widget');
 
 var SflSupport = Widget.extend({
     template:'standard_plus_issue.sfl_support',
+
+    events: {
+	click: 'on_click',
+    },
+
+    on_click: function(event) {
+	event.preventDefault();
+	this.do_action(
+	    'standard_plus_issue.action_standard_plus_issue_systray',
+	    {clear_breadcrumbs: true}
+	);
+    },
 });
 
 SflSupport.prototype.sequence = 900;
